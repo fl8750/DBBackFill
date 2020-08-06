9f62b5499cbb181a96ca8d403d4486e68839a28c
FredL <Flaforest@laforest.biz>
2020-08-05 20:36:21 -0400

    Modify the TableInfo class to include Views as well as Tables. The ObjType property will now hold a "V" when describing a view.

--- SqlServer/TableInfo.cs
+++ SqlServer/TableInfo.cs
@@ -38,6 +38,24 @@ namespace DBBackfill
 
         public int ObjectId { get; internal set; }
         public string ObjectType { get; internal set; }
+
+        public enum enumObjType
+        {
+            Table = 0,
+            View,
+            Unknown = 9999999
+        }
+        
+        public enumObjType ObjType
+        {
+            get
+            {
+                enumObjType result;
+                if (enumObjType.TryParse(ObjectType, out result)) return result;
+                return enumObjType.Unknown;
+            }
+        }
+
         public Int64 RowCount { get; set; }
 
         public bool HasIdentity { get; internal set; }
@@ -161,65 +179,70 @@ namespace DBBackfill
         string strTableColInfo = @"
     USE [{0}]; 
     WITH    IDXC
-          AS ( 		  
-			SELECT   *,
-						ROW_NUMBER() OVER (PARTITION BY IDXC2.[object_id], IDXC2.column_id ORDER BY IDXC2.IndexPriority) AS ColPrio
-               FROM     ( SELECT    DISTINCT
-									TBL.[object_id] ,
-                                    IDX.index_id ,
-									IDX.[type],
-                                    IDX.is_unique ,
-                                    COL.column_id ,
-                                    COALESCE(IDC.key_ordinal, 0) AS key_ordinal ,
-                                    COALESCE(IDC.is_descending_key , 0) AS is_descending_key,
-                                    COALESCE(IDC.partition_ordinal, 0) AS partition_ordinal
-                                    ,DENSE_RANK() OVER ( PARTITION BY TBL.[object_id] ORDER BY IDX.is_unique DESC, IDX.index_id ) AS IndexPriority
-	                         FROM (sys.objects TBL 
-		                        INNER JOIN sys.columns COL ON (TBL.[object_id] = COL.[object_id]))
-		                        LEFT OUTER JOIN 
-	                                (sys.indexes IDX
-		                                INNER JOIN sys.index_columns IDC 
-                                            ON ( IDX.[object_id] = IDC.[object_id] ) AND ( IDX.[index_id] = IDC.[index_id] )
-                                    )
-						        ON (TBL.[object_id] = IDX.[object_id] ) AND (COL.column_id = IDC.column_id)
-                            WHERE (TBL.[type] IN ('U','V'))
-                        ) IDXC2				
-             )
+              AS ( 		  
+			    SELECT   *,
+						    ROW_NUMBER() OVER (PARTITION BY IDXC2.[object_id], IDXC2.column_id ORDER BY IDXC2.IndexPriority) AS ColPrio
+                   FROM     ( SELECT    DISTINCT
+									    TBL.[object_id] ,
+									    --OBJECT_SCHEMA_NAME(TBL.[object_id]) AS SchemaName,
+									    --OBJECT_NAME(TBL.[object_id]) AS ObjectName,
+									    TBL.[type] AS ObjType,
+                                        COALESCE(IDX.index_id,0) AS IndexID ,
+									    --COALESCE(IDX.[type], 0) AS IndexType,
+                                        COALESCE(IDX.is_unique, 0) AS is_unique ,
+                                        COL.column_id ,
+                                        COALESCE(IDC.key_ordinal, 0) AS key_ordinal ,
+                                        COALESCE(IDC.is_descending_key , 0) AS is_descending_key,
+                                        COALESCE(IDC.partition_ordinal, 0) AS partition_ordinal
+                                        ,DENSE_RANK() OVER ( PARTITION BY TBL.[object_id] ORDER BY COALESCE(IDX.is_unique, 0) DESC, COALESCE(IDX.index_id,0) ) AS IndexPriority
+	                             FROM (sys.objects TBL 
+		                            INNER JOIN sys.columns COL ON (TBL.[object_id] = COL.[object_id]))
+		                            LEFT OUTER JOIN 
+	                                    (sys.indexes IDX
+		                                    INNER JOIN sys.index_columns IDC 
+                                                ON ( IDX.[object_id] = IDC.[object_id] ) AND ( IDX.[index_id] = IDC.[index_id] )
+                                        )
+						            ON (TBL.[object_id] = IDX.[object_id] ) AND (COL.column_id = IDC.column_id)
+                                WHERE (TBL.[type] IN ('U','V'))
+							    --ORDER BY TBL.object_id, COL.column_id
+                            ) IDXC2				
+                 )
 
-         SELECT   TAB.[object_id] ,
-                        TAB.[schema_id] ,
-                        SCH.name AS SchemaName ,
-                        TAB.name AS TableName ,
-                        IDX.[type] AS index_type ,
-                        IC.[name] AS ColName ,
-                        IC.column_id ,
-                        TYP.[name] AS DataType ,
-                        IC.max_length ,
-                        IC.[precision] ,
-                        IC.[scale] ,
-                        IC.is_xml_document ,
-                        IC.is_nullable ,
-                        IC.is_identity ,
-                        IC.is_computed ,
-                        COALESCE(IDXC.key_ordinal, 0) AS key_ordinal ,
-                        COALESCE(IDXC.is_descending_key, 0) AS is_descending_key ,
-                        COALESCE(IDXC.partition_ordinal, 0) AS partition_ordinal 
-               FROM     sys.objects TAB
-                        INNER JOIN sys.schemas SCH ON ( TAB.[schema_id] = SCH.[schema_id] )
-                        INNER JOIN sys.indexes IDX ON ( TAB.[object_id] = IDX.[object_id] )
-                        INNER JOIN sys.columns IC ON ( TAB.[object_id] = IC.[object_id] )
-                        INNER JOIN sys.types TYP ON ( IC.user_type_id = TYP.user_type_id )
-                        LEFT OUTER JOIN /* sys.index_columns */ IDXC ON ( TAB.[object_id] = IDXC.[object_id] )
-                                                                       -- AND ( IDX.index_id = IDXC.index_id )
-                                                                        AND ( IC.column_id = IDXC.column_id )
-               WHERE    ( TAB.is_ms_shipped = 0 )
-                        AND ( IDX.[type] IN ( 0, 1, 5 ) )
-						AND (IDXC.ColPrio = 1)
-                        AND (TAB.[type] IN ('U', 'V'))
+             SELECT   TAB.[object_id] ,
+                            TAB.[schema_id] ,
+                            SCH.name AS SchemaName ,
+                            TAB.name AS TableName ,
+						    TAB.[type] AS ObjType,
+                            COALESCE(IDX.[type],0) AS IndexType ,
+                            IC.[name] AS ColName ,
+                            IC.column_id ,
+                            TYP.[name] AS DataType ,
+                            IC.max_length ,
+                            IC.[precision] ,
+                            IC.[scale] ,
+                            IC.is_xml_document ,
+                            IC.is_nullable ,
+                            IC.is_identity ,
+                            IC.is_computed ,
+                            COALESCE(IDXC.key_ordinal, 0) AS key_ordinal ,
+                            COALESCE(IDXC.is_descending_key, 0) AS is_descending_key ,
+                            COALESCE(IDXC.partition_ordinal, 0) AS partition_ordinal 
+                   FROM     sys.objects TAB
+                            INNER JOIN sys.schemas SCH ON ( TAB.[schema_id] = SCH.[schema_id] )
+                            INNER JOIN sys.columns IC ON ( TAB.[object_id] = IC.[object_id] )
+                            INNER JOIN sys.types TYP ON ( IC.user_type_id = TYP.user_type_id )
+                            LEFT OUTER JOIN sys.indexes IDX ON ( TAB.[object_id] = IDX.[object_id] )
+                            LEFT OUTER JOIN /* sys.index_columns */ IDXC ON ( TAB.[object_id] = IDXC.[object_id] )
+                                                                           -- AND ( IDX.index_id = IDXC.index_id )
+                                                                            AND ( IC.column_id = IDXC.column_id )
+                   WHERE    ( TAB.is_ms_shipped = 0 )
+                            AND (TAB.[type] IN ('U', 'V'))
+                            AND ( ISNULL(IDX.[type], 0) IN ( 0, 1, 5 ) )
+						    AND (IDXC.ColPrio = 1)
 
-               ORDER BY SchemaName ,
-                        TableName ,
-                        column_id;";
+                   ORDER BY SchemaName ,
+                            TableName ,
+                            IC.column_id";
 
 
 
@@ -288,7 +311,7 @@ namespace DBBackfill
                         {
                             string schemaName = (string) dr["SchemaName"];
                             string tableName = (string) dr["TableName"];
-                            string objType = "U";
+                            string objType = (string)dr["ObjType"]; // "U"; or "V"
                             //string objType = (string)dr["ObjType"];
                             curTbl = new TableInfo(dbConn, dbConn.DataSource, dbName, schemaName, tableName, objectID, objType);
                             _tables.Add(curTbl.ObjectId, curTbl);
